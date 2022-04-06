53b4378331e805a5186c648ce76ed731d40f854a
noelensith <mbnoelen@gmail.com>
2022-04-05 23:01:45 -0700

    Complete Assignment

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(titleCased, tutorials)
+  function titleCased(tutorials) {
+    return tutorials.split(' ').map((word) => {
+      return word[0].toUpperCase() + word.slice(1)
+    }).join(' ')
+  }
 }
+
